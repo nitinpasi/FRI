5e78f144a05f2190c12c9c1e537b5f2a475430fb
sharmarochanexitgit config --global user.name sharmarochan <sharmarochan@gmail.com>
2018-06-26 17:59:59 +0530

    large data target fix image count

--- FRI.py
+++ FRI.py
@@ -3,7 +3,7 @@ import os, sys, csv
 import numpy as np
 from collections import Counter
 
-path = "/Users/sharmarochan/Desktop/extracted files/resized_images/"
+path = "/Users/sharmarochan/Desktop/extracted files/images_together/"
 dirs = os.listdir( path )
 dirs_array = np.array(dirs)
 dirs_array_sort = np.sort(dirs_array)
@@ -12,21 +12,26 @@ dirs_array_sort = np.sort(dirs_array)
 
 
 def resize():
-    with open('/Users/sharmarochan/Desktop/extracted files/num_data_resized_images.csv','w') as f:
+    with open('/Users/sharmarochan/Desktop/extracted files/num_images.csv','w') as f:
         writer = csv.writer(f)
         lable =1
-        loop_check = ""
-        pos =0
-        print(dirs_array_sort)
+        image = 0
         val_store = "Barking_deer"
+
+
+
         for item in dirs_array_sort:
+
             if not item.startswith('.') and item != 'Thumbs.db':
+                image = image + 1
+
                 par1, par2 = item.rsplit('_', 1)
 
                 if val_store != par1:
                     lable = lable + 1
 
                 val_store= par1
+                print(image)
 
 
             if not item.startswith('.') and item != 'Thumbs.db':
